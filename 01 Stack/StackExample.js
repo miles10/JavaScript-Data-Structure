09e678c65eae5da70d062214be157d2147962613
Miles Ma <msk10@foxmail.com>
2020-05-22 13:21:57 +0800

    利用栈完成十进制转二进制

--- 01 Stack/StackExample.js
+++ 01 Stack/StackExample.js
@@ -17,8 +17,23 @@ stack_temp.print()
 console.log("----------------------")
 console.log("十进制转二进制")
 
+// 利用栈完成十进制转二进制
 function dec2bin(decNumber){
     let stack = new Stack()
     let remainder;
     
+    while(decNumber > 0){
+        remainder = parseInt(decNumber / 2)
+        stack.push(decNumber % 2)
+        decNumber = remainder
+    }
+
+    let bin_Number="";
+    while(!stack.isEmpty()){
+        bin_Number = bin_Number + String(stack.pop())
+    }
+    return parseInt(bin_Number)
 }
+
+console.log( dec2bin(11))
+console.log(typeof(dec2bin(11)))
