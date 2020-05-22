0ca18449b19dbed76bfa58babc0477a2a2cc0377
Miles Ma <msk10@foxmail.com>
2020-05-22 17:27:30 +0800

    LinkedList append 方法

--- 03 LinkedList/LinkedListClass.js
+++ 03 LinkedList/LinkedListClass.js
@@ -18,91 +18,26 @@ function LinkedList(){
     // isEmpty()
     // size()
     // toString() 输出时 只输出元素值 不输出next
-    this.enqueue = function (element) {
-        items.push(element)
-    }
-
-    this.dequeue = function () {
-        return items.shift()
-    }
-
-    this.front = function () {
-        return items[0]
-    }
-
-    this.isEmpty = function () {
-        return items.length == 0
-    }
-
-    this.size = function () {
-        return items.length
-    }
-
-    this.print = function () {
-        console.log(items)
-    }
-}
-
-
-// 优先队列
-function PriorityQueue(){
-    let items = []
-
-    // 封装构造函数 保存元素 和元素优先级
-    function QueueElement(element, priority){
-        this.element = element;
-        this.priority = priority;
-    }
-
-
-    // 添加元素方法
-    this.enqueue = function (element, priority) {
-        let queueElement = new QueueElement(element, priority)
-        if(this.isEmpty()){
-            items.push(queueElement)
-        }else{
-            let added = false
-            for(let i = 0; i < items.length; i++){
-                if(queueElement.priority < items[i].priority ){
-                    items.splice(i, 0, queueElement)
-                    added = true
-                    break
-                }
-            }
-            // 结束循环 若元素未添加
-            if(added == false){
-                items.push(queueElement)
-            }
-        }
-    }
-
-    // 删除元素的方法
-    this.dequeue = function () {
-        return items.shift()
-    }
-
-    // 获取前端的元素
-    this.front = function () {
-        return items[0]
-    }
-
-    // 查看元素是否为空
-    this.isEmpty = function () {
-        return items.length == 0
-    }
-
-    // 获取元素的个数
-    this.size = function () {
-        return items.length
-    }
-
-    this.print = function () {
-        console.log(items)
-    }
     
+}
+// 链表尾部追加元素方法
+LinkedList.prototype.append = function (element) {
+    let newNode = new Node(element)
+
+    if(this.head === null){ // 列表为空
+        this.head = newNode
+    } else {  // 列表不为空
+        let current = this.head
+        while(current.next != null){
+            current = current.next
+        }
+        current.next = newNode
+    }
+
+    // 链表增长
+    this.length++
 }
 
 // 导出 Queue Class
-module.exports = {Queue: Queue, 
-                PriorityQueue: PriorityQueue};
+module.exports = {LinkedList: LinkedList};
 
